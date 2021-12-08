f28154d5fb7729e3d20dc2198efa4f2f5dea6407
Alexander Mehregan <amehregan@berkeley.edu>
2021-12-07 22:09:11 -0800

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Alex M on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
